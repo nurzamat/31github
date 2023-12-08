72142daa7ab5fdfa0138ec9f5f9b4e49d5718dc1
nurzamatnasyrkanov <nurzamat@gmail.com>
2023-12-08 21:09:00 +0600

    api urls

--- Taptap/api/urls.py
+++ Taptap/api/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
+    path('countries', views.countries, name='countries'),
+    path('regions', views.regions, name='regions'),
+    path('create_user', views.create_user, name='create_user'),
+    path('login', views.login, name='login'),
 ]
