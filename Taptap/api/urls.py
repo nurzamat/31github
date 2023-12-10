76301e68a98fe8aae6077bc4a1e688aea87e59fd
nurzamatnasyrkanov <nurzamat@gmail.com>
2023-12-10 19:00:12 +0600

    api urls for posts

--- Taptap/api/urls.py
+++ Taptap/api/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('regions', views.regions, name='regions'),
     path('create_user', views.create_user, name='create_user'),
     path('login', views.login, name='login'),
+    path('posts', views.posts, name='posts'),  # if type = 0 show all posts
+    path('api_user_posts', views.user_posts, name='api_user_posts'),
+    path('post_hitcount', views.post_hitcount, name='post_hitcount'),
+    path('search_post', views.search_post, name='search_post'),  # if type = 0 search in all posts
+    path('create_post', views.create_post, name='create_post'),
+    path('post_detail', views.post_detail, name='post_detail'),
 ]
